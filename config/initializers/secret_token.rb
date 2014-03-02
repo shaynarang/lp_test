6a01a722907ae5161a75c8624f1b3795cab63abc
shaynarang <shaynarang@gmail.com>
2014-03-02 09:52:07 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LpTest::Application.config.secret_key_base = '06072c27ee0e6f3568c13de732e71153f043aa11ae5dd55b64a43a01cd95e7e15fe24040011fdb09daa3d3efe0001ba656e0c9d5fa091d87460c9a4bd5881ace'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LpTest::Application.config.session_store :cookie_store, key: '_lp_test_session'
